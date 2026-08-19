3714d8188f707d57039328caeb8b43ac7c675192
Jhz-YZCS <2493719107@qq.com>
2026-08-19 22:07:04 +0800

    Fix collapsible quiz curriculum navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='zju336-v20';
+const CACHE='zju336-v21';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
